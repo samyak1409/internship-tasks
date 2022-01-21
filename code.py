409929b2478b76059725aa24b9de11210a957e3d
Samyak Jain <samyak65400@gmail.com>
2022-01-21 17:41:57 +0530

    part 1 completed!

--- code.py
+++ code.py
@@ -1,32 +1,47 @@
 from requests import get as get_request
 from bs4 import BeautifulSoup
+from itertools import count
 
 
 website = 'https://coinmarketcap.com'
 
-web_response = get_request(url=website)
-# print(web_response); input()  # debugging
 
-if web_response.status_code == 200:  # everything's okay
+for page_num in count(start=91):  # infinite loop
 
-    web_html = web_response.text
-    # print(web_html); input()  # debugging
+    webpage = f'{website}/?page={page_num}'
+    response = get_request(url=webpage)
+    print('\n', webpage, response, '\n')
 
-    soup = BeautifulSoup(markup=web_html, features='html.parser')
-    # print(soup.prettify()); input()  # debugging
+    if response.status_code == 200:  # everything's okay
 
-    tags = soup.find(name='tbody').find_all(name='div', class_='sc-16r8icm-0 escjiH')
-    for tag in tags:
-        # print(tag.prettify(), '\n'); continue  # debugging
+        web_html = response.text
+        # print(web_html); input()  # debugging
 
-        currency = tag.a.get('href')
-        # print(currency); continue  # debugging
+        soup = BeautifulSoup(markup=web_html, features='html.parser')  # https://www.crummy.com/software/BeautifulSoup/bs4/doc/#differences-between-parsers
+        # print(soup.prettify()); input()  # debugging
 
-        currency_web = website + currency
-        print(currency_web); continue  # debugging
+        tags = soup.find(name='tbody').find_all(class_=['sc-16r8icm-0 escjiH', 'sc-1rqmhtg-0 jUUSMS'])
+        for num, tag in enumerate(tags, start=1):
 
-        # MAIN:
+            num += (page_num-1) * 100
+            # print(tag.prettify(), '\n'); continue  # debugging
+
+            currency = tag.a['href']
+            # print(f'{num}) {currency}'); continue  # debugging
+
+            currency_web = website + currency
+            print(f'{num}) {currency_web}')
+
+            # MAIN:
+
+    elif response.status_code == 404:  # webpage not found
+        print('Stopping... \n')
+        break  # stop looping
+
+    else:
+        from time import sleep
+        sleep(1)
+        exit(response.reason)
 
 
-else:
-    print(web_response)
+print('SUCCESS')
