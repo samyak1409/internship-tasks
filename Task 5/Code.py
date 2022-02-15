a3c02c0d3e2d59a8d6593386e73c4e610defc595
Samyak Jain <samyak65400@gmail.com>
2022-02-15 17:34:13 +0530

    threading

--- Task 5/Code.py
+++ Task 5/Code.py
@@ -5,32 +5,51 @@ https://public-api.solscan.io/block/transactions?block=120304941&offset=0&limit=
 """
 
 
+# Requests: ~120M * 1
+# API Doc: https://public-api.solscan.io/docs/
+
+
 # IMPORTS:
 
 from requests import get as get_request
 from json import loads, dumps
+from concurrent.futures import ThreadPoolExecutor
+
+
+# CONSTANTS:
+
+DEBUG = True  # (default: False)
+THREADS = 1 if DEBUG else 150  # number of concurrent threads to run at once
+
+
+# FUNCTIONS:
+
+def get_data_from(url: str) -> dict:
+    data = loads(s=get_request(url=url).text)
+    if DEBUG:
+        print(url)
+        print(dumps(obj=data, indent=4))
+    return data
 
 
 # MAIN:
 
-START = 1
+def main(block_num: int) -> None:
 
-try:
-    stop = int(input('\nHOW MANY?: '))
-except ValueError as e:
-    raise SystemExit(e)
+    print(f"\n{block_num})")
 
-for block_num in range(START, START+stop):
+    get_data_from(url=f'https://public-api.solscan.io/block/transactions?block={block_num}')
 
-    url = f'https://public-api.solscan.io/block/transactions?block={block_num}&offset=0&limit=100000'
-    print(f'\n{block_num}) {url}')
 
-    data_dict = loads(s=get_request(url=url).text)
+# THREADING:
 
-    len_ = len(data_dict)
-    print('LEN:', len_)
-    if not len_:
-        print('MAX HEIGHT REACHED')
+if DEBUG:
+    print('\nGETTING LAST BLOCK NUMBER')  # spacing
+for page_num in range(1, get_data_from(url=f'https://public-api.solscan.io/block/last?limit=1')[0]['currentSlot']+1, THREADS):  # start, stop, step
+    with ThreadPoolExecutor() as Exec:
+        Exec.map(main, range(page_num, page_num+THREADS))
+    if DEBUG:
         break
 
-    print(dumps(obj=data_dict, indent=4))
+
+print('\nSUCCESS!')
