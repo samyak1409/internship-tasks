8b27bf9b5ef06b5d461fb7aafc8c479224c6914d
Samyak Jain <samyak65400@gmail.com>
2022-02-15 19:49:47 +0530

    ip_rotator

--- Task 5/Code.py
+++ Task 5/Code.py
@@ -11,21 +11,23 @@ https://public-api.solscan.io/block/transactions?block=120304941&offset=0&limit=
 
 # IMPORTS:
 
-from requests import get as get_request
+from requests import Session
 from json import loads, dumps
 from concurrent.futures import ThreadPoolExecutor
+from requests_ip_rotator import ApiGateway
 
 
 # CONSTANTS:
 
+BASE_URL = 'https://public-api.solscan.io'
 DEBUG = True  # (default: False)
-THREADS = 1 if DEBUG else 150  # number of concurrent threads to run at once
+THREADS = 1 if DEBUG else 15  # number of concurrent threads to run at once
 
 
 # FUNCTIONS:
 
 def get_data_from(url: str) -> dict:
-    data = loads(s=get_request(url=url).text)
+    data = loads(s=session.get(url=url).text)
     if DEBUG:
         print(url)
         print(dumps(obj=data, indent=4))
@@ -38,18 +40,25 @@ def main(block_num: int) -> None:
 
     print(f"\n{block_num})")
 
-    get_data_from(url=f'https://public-api.solscan.io/block/transactions?block={block_num}')
+    x = get_data_from(url=f'{BASE_URL}/block/transactions?block={block_num}')
+
+    if block_num % 75 == 0:
+        print(dumps(obj=x, indent=4))
 
 
-# THREADING:
+# THREADING WITH ROTATING IPs:
 
-if DEBUG:
-    print('\nGETTING LAST BLOCK NUMBER')  # spacing
-for page_num in range(1, get_data_from(url=f'https://public-api.solscan.io/block/last?limit=1')[0]['currentSlot']+1, THREADS):  # start, stop, step
-    with ThreadPoolExecutor() as Exec:
-        Exec.map(main, range(page_num, page_num+THREADS))
-    if DEBUG:
-        break
+with ApiGateway(BASE_URL) as g:  # https://github.com/Ge0rg3/requests-ip-rotator/
+    session = Session()
+    session.mount(BASE_URL, g)
+
+    print('\nGETTING LAST BLOCK NUMBER')
+
+    for page_num in range(1, get_data_from(url=f'{BASE_URL}/block/last?limit=1')[0]['currentSlot']+1, THREADS):  # start, stop, step
+        with ThreadPoolExecutor() as Exec:  # https://youtu.be/IEEhzQoKtQU
+            Exec.map(main, range(page_num, page_num+THREADS))
+        if DEBUG:
+            break
 
 
 print('\nSUCCESS!')
