70094c0ad566ca0dd4cec1f5726231eba44c42b2
Samyak Jain <samyak65400@gmail.com>
2022-04-10 22:17:41 +0530

    10) init

---
+++ 10) GitLab/Code.py
@@ -0,0 +1,18 @@
+"""
+Hi Samyak,
+
+We will need data (commits, developers' details, etc.) for every repo in the following GitLab repositories:
+https://gitlab.com/tezos/tezos
+https://gitlab.com/bitcoin-cash-node/bitcoin-cash-node
+https://gitlab.com/NebulousLabs
+
+I have attached the GitHub code I used to get the data from GitHub repositories.
+
+Please keep in mind that an org has multiple repos, you will need to fetch the orgs for the repos before going to the
+final step of getting commits data.
+
+I have also attached sample dataset of how the final data should look like.
+
+Regards,
+Vasundhara
+"""
---
+++ 10) GitLab/README.md
@@ -0,0 +1,8 @@
+# Task 10
+
+
+
+
+## Time required to pull the data
+
+
